6597a7b969d381ce734caa4333a05e2b95e98bbe
dhruv0703 <115712795+dhruv0703@users.noreply.github.com>
2023-08-05 22:43:32 +0530

    Updates

---
+++ precache-manifest.4327155064a1de66196c96f8270d316c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daaa29ce245cf3b23b4f4d28f537ccf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3405dbc5aa2ee52cfe79",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "eb17c503aaa5423bfc40",
+    "url": "/static/js/2.4a9627e8.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.4a9627e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3405dbc5aa2ee52cfe79",
+    "url": "/static/js/main.8efd8934.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
